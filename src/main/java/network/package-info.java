596772ce60938727ff1e77031b254694ce30cefe
WinsonHuang <hwangwc@qq.com>
2023-08-21 18:26:18 +0800

    Add package-info

---
+++ src/main/java/network/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Network feature demo
+ * <p>
+ * This package contains demos of Java SE Network feature (mostly from Core Java
+ * 2 Chap4), subpackage {@code highlevel} is about 4.4 without {@code URLConnection}.
+ * </p>
+ * 
+ * @author Winson Huang
+ */
+package network;
